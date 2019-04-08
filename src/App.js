2e3183f456a08145eacedcd5a55f5742fc55c1fa
paristuzun <paristuzun@gmail.com>
2019-04-07 22:41:58 -0400

    added a fetch request to get hosts and areas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import './stylesheets/App.css'
 import { Segment } from 'semantic-ui-react';
+import WestworldMap from './components/WestworldMap'
+import Headquarters from './components/Headquarters'
+import Log from './services/Log'
 
 
 class App extends Component {
 
-  // As you go through the components given you'll see a lot of functional components.
-  // But feel free to change them to whatever you want.
-  // It's up to you whether they should be stateful or not.
+  state = {
+    areas: [],
+    hosts: [],
+    clickedHost: null,
+    logs: []
+  }
+
+  componentDidMount = () => {
+    fetch('http://localhost:4000/areas')
+      .then(res => res.json())
+      .then(areas => this.setState({areas}))
+      
+    fetch('http://localhost:4000/hosts')
+      .then(res => res.json())
+      .then(hosts => this.setState({hosts}))
+  }
 
   render(){
     return (
